cd6c5aed0f4ed7d49352848e4255470aca1099b4
Fabian Yanez <fabian.yzb@gmail.com>
2024-02-29 06:27:15 -0300

    feat

--- app-notas/src/App.jsx
+++ app-notas/src/App.jsx
@@ -1,32 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+
+  const getNotas = () => {
+    fetch("http://localhost:4000/notas")
+      /* cuando el ejecuta la peticion o promesa de fetch despues viene el .then que manipulara la promesa
+      luego del .then viene una funcion que se ejecutara al momento de recibir la respuesta (response) del servidor.
+      Como estamos trabajando en formato json las respuestas seran en json  */
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => { })
+  }
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        HOLA
       </p>
     </>
   )
